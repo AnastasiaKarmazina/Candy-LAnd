8f82e8c01d483c737b8ee4088b8c428f182da755
Anastasia <nkarmazina1996@gmail.com>
2023-04-04 14:10:38 +0300

    Info Product

---
+++ src/product/ProductInfo.java
@@ -0,0 +1,32 @@
+package product;
+
+public class ProductInfo {
+
+    private String name;
+    private int amount;
+    private double tariff;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public double getTariff() {
+        return tariff;
+    }
+
+    public void setTariff(double tariff) {
+        this.tariff = tariff;
+    }
+}
