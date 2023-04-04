f2b45d79fa1dc82caf743addff5aecd116e06407
Anastasia <nkarmazina1996@gmail.com>
2023-04-04 14:50:01 +0300

    Models add  Card Tax model

--- src/models/CardTaxModel.java
+++ src/models/CardTaxModel.java
@@ -1,4 +1,24 @@
 package models;
 
-public class CardTaxModel {
+import calculateTax.CardTax;
+
+public class CardTaxModel extends DiscountModel implements CardTax {
+
+    public CardTaxModel(double calculatePrice, double calculateDiscount, double calculatePriceWithDiscount){
+        super();
+    }
+    @Override
+    public double calculateCardTax(double priceWithDiscount){
+        if(priceWithDiscount <= 5000){
+            return priceWithDiscount * 3 / 100;
+        }else if(priceWithDiscount > 5000 && priceWithDiscount <= 10000){
+            return priceWithDiscount * 2 / 100;
+        }else if(priceWithDiscount > 10000){
+            return priceWithDiscount;
+        }
+        return calculateCardTax(priceWithDiscount);
+    }
+    public double calculateTaxWithCardCost(double priceWithDiscount, double cardTax){
+        return priceWithDiscount + cardTax;
+    }
 }
