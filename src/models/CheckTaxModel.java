7fe3af8faf3c1abc402841662261be0b822a8ec3
Anastasia <nkarmazina1996@gmail.com>
2023-04-04 14:55:34 +0300

    Models add  Check Tax model

--- src/models/CheckTaxModel.java
+++ src/models/CheckTaxModel.java
@@ -1,4 +1,17 @@
 package models;
 
-public class CheckTaxModel {
+import calculateTax.CheckTax;
+
+public class CheckTaxModel extends DiscountModel implements CheckTax {
+
+    public CheckTaxModel(double calculatePrice, double calculateDiscount, double calculatePriceWithDiscount){
+        super();
+    }
+    @Override
+    public double calculateCheckTax(double priceWithDiscount){
+        return priceWithDiscount * 2 / 100;
+    }
+    public double calculateTaxCheckCost(double priceWithDiscount, double checkTax){
+        return priceWithDiscount + checkTax;
+    }
 }
